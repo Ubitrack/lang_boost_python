0c04c1b502d236b38d5c57fcdac5eb2feae1cc81
Ulrich Eck <ulrich.eck@magicvisionlab.com>
2013-11-01 17:20:54 +1030

    disable dataflow tests for now .. they're abstract and cannot be tested directly..

--- tests/test_utDataflow.py
+++ tests/test_utDataflow.py
@@ -17,21 +17,22 @@ def teardown_func():
 
 
 
-@with_setup(setup_func, teardown_func)
-def test_basic_dataflow_components():
-    "test basic dataflow components"
-
-    c = dataflow.Component("test1")
-    assert c.getName() == "test1"
-    # XXX adding ports results in segfaults cause by double freeing a pointer
-    # eventually, Component/Port will become abstract classes, that cannot be instantiated 
-    # from within python .. also makes sense somehow..
+# @with_setup(setup_func, teardown_func)
+# def test_basic_dataflow_components():
+#     "test basic dataflow components"
+# 
+#     # abstract classes for now
+#     c = dataflow.Component("test1")
+#     assert c.getName() == "test1"
+#     # XXX adding ports results in segfaults cause by double freeing a pointer
+#     # eventually, Component/Port will become abstract classes, that cannot be instantiated 
+#     # from within python .. also makes sense somehow..
 #     p1 = dataflow.Port("P1", c)
 #     assert p1.fullName() == "test1:P1"
-    c1 = dataflow.Component("test2")
-#    p2 = dataflow.Port("P2", c1)
+#     c1 = dataflow.Component("test2")
+#     p2 = dataflow.Port("P2", c1)
 #     assert p2.fullName() == "test2:P2"
-    
+#       
 #     p1.connect(p2)
 #     c1.start()
 #     c.start()
