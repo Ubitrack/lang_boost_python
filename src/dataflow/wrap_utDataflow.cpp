f35a62d078af1a114a9a654ae4577feb533424d3
Ulrich Eck <ulrich.eck@magicvisionlab.com>
2013-11-01 16:00:26 +1030

    continue from home..

--- src/dataflow/wrap_utDataflow.cpp
+++ src/dataflow/wrap_utDataflow.cpp
@@ -13,6 +13,8 @@
 #include <utDataflow/DataflowNetwork.h>
 #include <utDataflow/Port.h>
 
+#include <utDataflow/PushConsumer.h>
+
 
 #include <iostream>
 
@@ -28,34 +30,33 @@ namespace {
 
 BOOST_PYTHON_MODULE(_utdataflow)
 {
-	bp::class_< Dataflow::Component, boost::shared_ptr< Dataflow::Component >, boost::noncopyable>("Component", bp::init< const std::string& >())
-			.def("getName", &Dataflow::Component::getName
-					,bp::return_value_policy<bp::copy_const_reference>()
-					)
-			.def("addPort", &Dataflow::Component::addPort)
-			.def("removePort", &Dataflow::Component::removePort)
-			.def("getPortByName", &Dataflow::Component::getPortByName
-					,bp::return_internal_reference<>()
-					)
-			.def("start", &Dataflow::Component::start)
-			.def("stop", &Dataflow::Component::stop)
-			.def("setEventPriority", &Dataflow::Component::setEventPriority)
-			.def("getEventPriority", &Dataflow::Component::getEventPriority)
-			;
+	bp::class_< Dataflow::Component, boost::shared_ptr< Dataflow::Component >, boost::noncopyable>("Component", bp::no_init)
+		.def("getName", &Dataflow::Component::getName
+				,bp::return_value_policy<bp::copy_const_reference>()
+				)
+		.def("addPort", &Dataflow::Component::addPort)
+		.def("removePort", &Dataflow::Component::removePort)
+		.def("getPortByName", &Dataflow::Component::getPortByName
+				,bp::return_internal_reference<>()
+				)
+		.def("start", &Dataflow::Component::start)
+		.def("stop", &Dataflow::Component::stop)
+		.def("setEventPriority", &Dataflow::Component::setEventPriority)
+		.def("getEventPriority", &Dataflow::Component::getEventPriority)
+		;
 
-	bp::class_< Dataflow::Port, boost::shared_ptr< Dataflow::Port >, boost::noncopyable>("Port", bp::init<const std::string&, Dataflow::Component& >())
-			.def("getName", &Dataflow::Port::getName
-					,bp::return_value_policy<bp::copy_const_reference>()
-					)
-			.def("fullName", &Dataflow::Port::fullName
-					//,bp::return_value_policy<bp::manage_new_object>()
-					)
-			.def("getComponent", &Dataflow::Port::getComponent
-					,bp::return_internal_reference<>()
-					)
-			.def("connect", &Dataflow::Port::connect)
-			.def("disconnect", &Dataflow::Port::disconnect)
-			;
-
+	bp::class_< Dataflow::Port, boost::shared_ptr< Dataflow::Port >, boost::noncopyable>("Port", bp::no_init)
+		.def("getName", &Dataflow::Port::getName
+				,bp::return_value_policy<bp::copy_const_reference>()
+				)
+		.def("fullName", &Dataflow::Port::fullName
+				//,bp::return_value_policy<bp::manage_new_object>()
+				)
+		.def("getComponent", &Dataflow::Port::getComponent
+				,bp::return_internal_reference<>()
+				)
+		.def("connect", &Dataflow::Port::connect)
+		.def("disconnect", &Dataflow::Port::disconnect)
+		;
 
 }
--- src/facade/wrap_utFacade.cpp
+++ src/facade/wrap_utFacade.cpp
@@ -3,6 +3,8 @@
 #include <numpy/arrayobject.h>
 #include <complex>
 
+#include <boost/bind.hpp>
+
 #include <boost/python.hpp>
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
@@ -10,6 +12,9 @@
 #include <boost/python/to_python_converter.hpp>
 
 #include <utFacade/AdvancedFacade.h>
+#include <utComponents/ApplicationPushSink.h>
+#include <utComponents/ApplicationPullSink.h>
+#include <utComponents/ApplicationPushSource.h>
 
 
 #include <iostream>
@@ -21,11 +26,66 @@ namespace bn = boost::numpy;
 
 namespace {
 
+//
+//template< typename T >
+//void pushconsumer_setCallback(bp::object const & cb) {
+//
+//
+//}
+//
+//
+//struct expose_pushconsumer
+//{
+//	template< class EventType >
+//    static void expose(const std::string& name)
+//    {
+//		bp::class_< Dataflow::ApplicationPushSink< EventType >, boost::shared_ptr< Dataflow::ApplicationPushSink< EventType >  >(name, bp::no_init)
+//			.def("setCallback", &get_measurement< T>
+//					//,return_value_policy<reference_existing_object>()
+//					,bp::return_value_policy<bp::copy_const_reference>()
+//					//,return_internal_reference<>()
+//					)
+//            ;
+//    }
+//};
+
+
+//template< class EventType >
+//struct apppushsink_exposer
+//{
+//    template <class C>
+//    static void expose(C const& c)
+//    {
+//        const_cast<C&>(c)
+//				.def(bp::init<Measurement::Timestamp>())
+//				.def(bp::init<Measurement::Timestamp, const typename T:: value_type& >())
+//				.def("time", (Measurement::Timestamp (T::*)() const)&T::time)
+//				.def("set_time", (void (T::*)(Measurement::Timestamp))&T::time)
+//				.def("invalid", (bool (T::*)())&T::invalid)
+//				.def("invalidate", (void (T::*)())&T::invalidate)
+//				.def("get", &get_measurement< T>
+//						//,return_value_policy<reference_existing_object>()
+//        				,bp::return_value_policy<bp::copy_const_reference>()
+//						//,return_internal_reference<>()
+//						)
+//				.def(bp::self_ns::str(bp::self_ns::self))
+//            ;
+//    }
+//};
+
+
+
+
 }
 
 
 BOOST_PYTHON_MODULE(_utfacade)
 {
+	bp::class_< Components::ApplicationPushSink< Measurement::Button >, boost::shared_ptr< Components::ApplicationPushSink< Measurement::Button > >, bp::bases< Dataflow::Component >, boost::noncopyable>("ApplicationPushSinkButton", bp::no_init)
+		.def("setCallback", &Components::ApplicationPushSink< Measurement::Button >::setCallback)
+		;
+
+
 	bp::class_< Facade::AdvancedFacade, boost::shared_ptr< Facade::AdvancedFacade >, boost::noncopyable>("AdvancedFacade", bp::init< bp::optional< const std::string& > >())
 		.def("loadDataflow", (void (Facade::AdvancedFacade::*)(const std::string&, bool))&Facade::AdvancedFacade::loadDataflow)
 		.def("loadDataflow", (void (Facade::AdvancedFacade::*)(std::istream&, bool))&Facade::AdvancedFacade::loadDataflow)
