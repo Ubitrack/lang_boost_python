befc4a34da4e7ab84a32a3116b054072a0c60917
Ulrich Eck <ulrich.eck@magicvisionlab.com>
2013-11-25 12:50:25 +1030

    basic image conversion works .. but needs more improvements

--- src/vision/wrap_utVision.cpp
+++ src/vision/wrap_utVision.cpp
@@ -1,3 +1,9 @@
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include <boost/numpy.hpp>
+#include <numpy/arrayobject.h>
+#include <complex>
+
+
 #include <boost/python.hpp>
 #include <boost/python/implicit.hpp>
 #include <boost/python/module.hpp>
@@ -25,7 +31,6 @@
 #include <utMeasurement/Measurement.h>
 #include <utVision/Image.h>
 
-#include "numpy/ndarrayobject.h"
 #include "opencv2/core/core.hpp"
 
 #include <ubitrack_python/wrap_streambuf.h>
@@ -34,284 +39,47 @@ using namespace Ubitrack;
 using namespace Ubitrack::Python;
 
 namespace bp = boost::python;
+namespace bn = boost::numpy;
 
 namespace {
 
-// The following conversion functions are taken from OpenCV's cv2.cpp file inside modules/python/src2 folder.
-static PyObject* opencv_error = 0;
+// this code will copy the data .. maybe there is a better way to do it
+// image-measurement is a parent, whose visibility can be used to manage the array data.
 
-static int failmsg(const char *fmt, ...)
-{
-    char str[1000];
+template< typename T >
+static bn::ndarray convert_image_as_array(Measurement::ImageMeasurement const & im) {
+    bn::dtype typenum = bn::dtype::get_builtin<T>();
 
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(str, sizeof(str), fmt, ap);
-    va_end(ap);
+    unsigned int frame_bytes = im->width * im->height * im->nChannels;
 
-    PyErr_SetString(PyExc_TypeError, str);
-    return 0;
-}
-
-class PyAllowThreads
-{
-public:
-    PyAllowThreads() : _state(PyEval_SaveThread()) {}
-    ~PyAllowThreads()
-    {
-        PyEval_RestoreThread(_state);
-    }
-private:
-    PyThreadState* _state;
-};
-
-class PyEnsureGIL
-{
-public:
-    PyEnsureGIL() : _state(PyGILState_Ensure()) {}
-    ~PyEnsureGIL()
-    {
-        PyGILState_Release(_state);
-    }
-private:
-    PyGILState_STATE _state;
-};
-
-#define ERRWRAP2(expr) \
-try \
-{ \
-    PyAllowThreads allowThreads; \
-    expr; \
-} \
-catch (const cv::Exception &e) \
-{ \
-    PyErr_SetString(opencv_error, e.what()); \
-    return 0; \
-}
-
-using namespace cv;
-
-static PyObject* failmsgp(const char *fmt, ...)
-{
-  char str[1000];
-
-  va_list ap;
-  va_start(ap, fmt);
-  vsnprintf(str, sizeof(str), fmt, ap);
-  va_end(ap);
-
-  PyErr_SetString(PyExc_TypeError, str);
-  return 0;
-}
-
-static size_t REFCOUNT_OFFSET = (size_t)&(((PyObject*)0)->ob_refcnt) +
-    (0x12345678 != *(const size_t*)"\x78\x56\x34\x12\0\0\0\0\0")*sizeof(int);
-
-static inline PyObject* pyObjectFromRefcount(const int* refcount)
-{
-    return (PyObject*)((size_t)refcount - REFCOUNT_OFFSET);
-}
-
-static inline int* refcountFromPyObject(const PyObject* obj)
-{
-    return (int*)((size_t)obj + REFCOUNT_OFFSET);
-}
-
-class NumpyAllocator : public MatAllocator
-{
-public:
-    NumpyAllocator() {}
-    ~NumpyAllocator() {}
-
-    void allocate(int dims, const int* sizes, int type, int*& refcount,
-                  uchar*& datastart, uchar*& data, size_t* step)
-    {
-        PyEnsureGIL gil;
-
-        int depth = CV_MAT_DEPTH(type);
-        int cn = CV_MAT_CN(type);
-        const int f = (int)(sizeof(size_t)/8);
-        int typenum = depth == CV_8U ? NPY_UBYTE : depth == CV_8S ? NPY_BYTE :
-                      depth == CV_16U ? NPY_USHORT : depth == CV_16S ? NPY_SHORT :
-                      depth == CV_32S ? NPY_INT : depth == CV_32F ? NPY_FLOAT :
-                      depth == CV_64F ? NPY_DOUBLE : f*NPY_ULONGLONG + (f^1)*NPY_UINT;
-        int i;
-        npy_intp _sizes[CV_MAX_DIM+1];
-        for( i = 0; i < dims; i++ )
-        {
-            _sizes[i] = sizes[i];
-        }
-
-        if( cn > 1 )
-        {
-            /*if( _sizes[dims-1] == 1 )
-                _sizes[dims-1] = cn;
-            else*/
-                _sizes[dims++] = cn;
-        }
-
-        PyObject* o = PyArray_SimpleNew(dims, _sizes, typenum);
-
-        if(!o)
-        {
-            CV_Error_(CV_StsError, ("The numpy array of typenum=%d, ndims=%d can not be created", typenum, dims));
-        }
-        refcount = refcountFromPyObject(o);
-
-        npy_intp* _strides = PyArray_STRIDES(o);
-        for( i = 0; i < dims - (cn > 1); i++ )
-            step[i] = (size_t)_strides[i];
-        datastart = data = (uchar*)PyArray_DATA(o);
+    bp::tuple shape;
+    if (im->nChannels == 1) {
+    	shape = bp::make_tuple(im->dimension().height, im->dimension().width);
+    } else if (im->nChannels == 3) {
+    	shape = bp::make_tuple(im->dimension().height, im->dimension().width, 3);
     }
 
-    void deallocate(int* refcount, uchar*, uchar*)
-    {
-        PyEnsureGIL gil;
-        if( !refcount )
-            return;
-        PyObject* o = pyObjectFromRefcount(refcount);
-        Py_INCREF(o);
-        Py_DECREF(o);
-    }
-};
-
-NumpyAllocator g_numpyAllocator;
-
-
-
-static PyObject* pyopencv_from(const Mat& m)
-{
-    if( !m.data )
-        Py_RETURN_NONE;
-    Mat temp, *p = (Mat*)&m;
-    if(!p->refcount || p->allocator != &g_numpyAllocator)
-    {
-        temp.allocator = &g_numpyAllocator;
-        m.copyTo(temp);
-        p = &temp;
-    }
-    p->addref();
-    return pyObjectFromRefcount(p->refcount);
+	bn::ndarray ret = bn::zeros(shape, typenum);
+	unsigned char* srcData = (unsigned char*) im->imageData;
+	unsigned char* dstData = (unsigned char*) ret.get_data();
+	memcpy(dstData, srcData, sizeof(unsigned char)*frame_bytes);
+	return ret;
 }
 
 
-enum { ARG_NONE = 0, ARG_MAT = 1, ARG_SCALAR = 2 };
-
-static int pyopencv_to(const PyObject* o, Mat& m, const char* name = "<unknown>", bool allowND=true)
-{
-    //NumpyAllocator g_numpyAllocator;
-    if(!o || o == Py_None)
-    {
-        if( !m.data )
-            m.allocator = &g_numpyAllocator;
-        return true;
+static bn::ndarray get_image_as_array(bp::object const & self) {
+	Measurement::ImageMeasurement im = bp::extract<Measurement::ImageMeasurement const &>(self)();
+    switch (im->depth) {
+    case CV_16U:
+    	return convert_image_as_array<unsigned short>(im);
+    	break;
+    case CV_32F:
+    	return convert_image_as_array<float>(im);
+    	break;
+    default:
+    	// assume CV8U is the default
+    	return convert_image_as_array<unsigned char>(im);
     }
-
-    if( !PyArray_Check(o) )
-    {
-        failmsg("%s is not a numpy array", name);
-        return false;
-    }
-
-    int typenum = PyArray_TYPE(o);
-    int type = typenum == NPY_UBYTE ? CV_8U : typenum == NPY_BYTE ? CV_8S :
-               typenum == NPY_USHORT ? CV_16U : typenum == NPY_SHORT ? CV_16S :
-               typenum == NPY_INT || typenum == NPY_LONG ? CV_32S :
-               typenum == NPY_FLOAT ? CV_32F :
-               typenum == NPY_DOUBLE ? CV_64F : -1;
-
-    if( type < 0 )
-    {
-        failmsg("%s data type = %d is not supported", name, typenum);
-        return false;
-    }
-
-    int ndims = PyArray_NDIM(o);
-    if(ndims >= CV_MAX_DIM)
-    {
-        failmsg("%s dimensionality (=%d) is too high", name, ndims);
-        return false;
-    }
-
-    int size[CV_MAX_DIM+1];
-    size_t step[CV_MAX_DIM+1], elemsize = CV_ELEM_SIZE1(type);
-    const npy_intp* _sizes = PyArray_DIMS(o);
-    const npy_intp* _strides = PyArray_STRIDES(o);
-    bool transposed = false;
-
-    for(int i = 0; i < ndims; i++)
-    {
-        size[i] = (int)_sizes[i];
-        step[i] = (size_t)_strides[i];
-    }
-
-    if( ndims == 0 || step[ndims-1] > elemsize ) {
-        size[ndims] = 1;
-        step[ndims] = elemsize;
-        ndims++;
-    }
-
-    if( ndims >= 2 && step[0] < step[1] )
-    {
-        std::swap(size[0], size[1]);
-        std::swap(step[0], step[1]);
-        transposed = true;
-    }
-
-    if( ndims == 3 && size[2] <= CV_CN_MAX && step[1] == elemsize*size[2] )
-    {
-        ndims--;
-        type |= CV_MAKETYPE(0, size[2]);
-    }
-
-    if( ndims > 2 && !allowND )
-    {
-        failmsg("%s has more than 2 dimensions", name);
-        return false;
-    }
-
-    m = Mat(ndims, size, type, PyArray_DATA(o), step);
-
-    if( m.data )
-    {
-        m.refcount = refcountFromPyObject(o);
-        m.addref(); // protect the original numpy array from deallocation
-                    // (since Mat destructor will decrement the reference counter)
-    };
-    m.allocator = &g_numpyAllocator;
-
-    if( transposed )
-    {
-        Mat tmp;
-        tmp.allocator = &g_numpyAllocator;
-        transpose(m, tmp);
-        m = tmp;
-    }
-    return true;
-}
-
-
-// Converts a matrix < n, m > to numpy array (copy)
-struct image_to_ndarray {
-	static PyObject* convert(Vision::Image const& p) {
-		const Mat m(p);
-		return pyopencv_from(m);
-	}
-
-};
-
-struct image_to_python_converter {
-	image_to_python_converter& to_python() {
-		bp::to_python_converter<Vision::Image,
-				image_to_ndarray, false //image_to_array has no get_pytype
-		>();
-		return *this;
-	}
-};
-
-static PyObject* get_image_as_array(bp::object const & self) {
-	const Mat m(bp::extract<Measurement::ImageMeasurement const &>(self)().get());
-	return pyopencv_from(m);
 }
 
 } // end anon ns
@@ -319,7 +87,10 @@ static PyObject* get_image_as_array(bp::object const & self) {
 
 BOOST_PYTHON_MODULE(_utvision)
 {
-	image_to_python_converter().to_python();
+	// initialize boost.numpy
+	bn::initialize();
+
+	//image_to_python_converter().to_python();
 
 	bp::class_<Measurement::ImageMeasurement>("ImageMeasurement")
 		.def(bp::init<Measurement::Timestamp>())
