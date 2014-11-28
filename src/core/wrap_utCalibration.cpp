e054eae4ac83ee88113d2e10012042c63f8da5f8
Ulrich Eck <ulrich.eck@magicvisionlab.com>
2014-11-28 17:55:38 +1030

    added wrappers for tipCalibration, tipCalibrationPose, absoluteOrientation, absoluteOrientationError

--- src/core/wrap_utCalibration.cpp
+++ src/core/wrap_utCalibration.cpp
@@ -25,7 +25,14 @@
 #include <streambuf>
 #include <iostream>
 
+#include <utUtil/Exception.h>
+#include <utMath/ErrorPose.h>
 #include <utAlgorithm/Projection.h>
+#include <utAlgorithm/ToolTip/TipCalibration.h>
+#include <utMath/VectorFunctions.h>
+#include <utAlgorithm/PoseEstimation3D3D/AbsoluteOrientation.h>
+#include <utAlgorithm/PoseEstimation3D3D/CovarianceEstimation.h>
+
 
 using namespace Ubitrack;
 
@@ -43,6 +50,58 @@ Math::Matrix< double, 4, 4 > projectionMatrix3x4ToOpenGL( double l, double r, do
 	return Algorithm::projectionMatrixToOpenGL( l, r, b, t, n, f, m );
 }
 
+Math::Vector< double, 3 > tipCalibration( const std::vector< Ubitrack::Math::Pose > &poses) {
+	Ubitrack::Math::Vector< double, 3 > pm;
+	Ubitrack::Math::Vector< double, 3 > pw;
+	Algorithm::ToolTip::tipCalibration(poses, pm, pw);
+	return pm;
+}
+
+Math::Pose tipCalibrationPose( const std::vector< Ubitrack::Math::Pose > &poses) {
+	Ubitrack::Math::Vector< double, 3 > pm;
+	Ubitrack::Math::Vector< double, 3 > pw;
+	Algorithm::ToolTip::tipCalibration(poses, pm, pw);
+
+	double length = norm_2(pm);
+	Math::Vector< double, 3 > refPos = -pm / length;
+	Math::Vector< double, 3 > errPos(0,0,1);
+
+	// 1. Calc normalized correction axis
+	Math::Vector< double, 3 > axis = cross_product(errPos, refPos);
+	// 2. Calc the correction angle
+	double angle = acos (inner_prod(errPos, refPos) / ( norm_2(errPos) * norm_2(refPos)));
+	// 3. Calc orientational correction. The axis will be normed by the constructor
+	Math::Quaternion corrRot( axis, angle);
+
+	Math::Pose p(corrRot, pm);
+	return p;
+}
+
+Math::Pose estimatePose6D_3D3D(const std::vector< Ubitrack::Math::Vector< double, 3> > &pointsA, const std::vector< Ubitrack::Math::Vector< double, 3> > &pointsB) {
+	Math::Pose resultPoseBA;
+	bool success = Algorithm::PoseEstimation3D3D::estimatePose6D_3D3D(pointsB, resultPoseBA, pointsA);
+	if (!success) {
+		UBITRACK_THROW( "Cannot estimate a result pose, some problem occured while performing a 3d-3d pose estimation (absolute orientation)");
+	}
+	return resultPoseBA;
+}
+
+Math::ErrorPose estimatePose6D_3D3D_Error(const std::vector< Ubitrack::Math::Vector< double, 3> > &pointsA, const std::vector< Ubitrack::Math::Vector< double, 3> > &pointsB) {
+	Math::Pose resultPoseBA;
+	bool success = Algorithm::PoseEstimation3D3D::estimatePose6D_3D3D(pointsB, resultPoseBA, pointsA);
+	if (!success) {
+		UBITRACK_THROW( "Cannot estimate a result pose, some problem occured while performing a 3d-3d pose estimation (absolute orientation) ");
+	}
+
+	Ubitrack::Math::Matrix< double, 6, 6> cov = Ubitrack::Math::Matrix< double, 6, 6>::zeros();
+	success = Algorithm::PoseEstimation3D3D::estimatePose6DCovariance(pointsA.begin(), pointsA.end(), resultPoseBA, pointsB.begin(), pointsB.end(), cov);
+	if (!success) {
+		UBITRACK_THROW( "Cannot estimate a result, some problem occured while calculating the covariance");
+	}
+
+	Ubitrack::Math::ErrorPose ep(resultPoseBA, cov);
+	return ep;
+}
 
 } // end anon ns
 
@@ -54,5 +113,8 @@ BOOST_PYTHON_MODULE(_utcalibration)
 
 	bp::def("projectionMatrix3x3ToOpenGL", &projectionMatrix3x3ToOpenGL);
 	bp::def("projectionMatrix3x4ToOpenGL", &projectionMatrix3x4ToOpenGL);
-
+	bp::def("tipCalibration", &tipCalibration);
+	bp::def("tipCalibrationPose", &tipCalibrationPose);
+	bp::def("absoluteOrientation", &estimatePose6D_3D3D);
+	bp::def("absoluteOrientationError", &estimatePose6D_3D3D_Error);
 }
